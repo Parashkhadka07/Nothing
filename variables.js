bc2cd58bb550a9a16e02ada94f3f961da8719cf0
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-18 17:35:36 +0545

    learned about variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+// es5
+//var a = "parash"; // global variable and orerrides old variables data
+
+var p = "ra";
+var p = "pa";
+console.log(p);
+
+//es6
+//const b = "ram"; // diclares fixed variable name
+
+cons g = "e";
+cons g = "f";
+console.log(g);
+
+//let c = "sita"; // let us update the old variables
+//console.log(a, b, c);
+
+// always start variables key word with cons keyword
