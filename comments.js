2109fcf7a5b93ccee14af71f485a7b53eb9f4e62
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-18 17:36:02 +0545

    learned about comments

---
+++ comments.js
@@ -0,0 +1,13 @@
+// single line comment
+by; //
+// this is a single line comment
+
+/* this is multi line comments
+parash
+khadka
+*/
+
+// this is also multi line comment ( ctrl + /) after selecting all the data
+// r
+// p
+// k
