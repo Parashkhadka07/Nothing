b73f9651fd93a0650a952855d0370194fe5c58be
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-18 17:54:00 +0545

    learned about interactions

---
+++ interactions.js
@@ -0,0 +1,6 @@
+alert("hello world");
+prompt(" how are you");
+confirm("are you male");
+`` this is called string litral
+ Assingment 
+ WAP a javascript program to create sum calculator using interactions.
