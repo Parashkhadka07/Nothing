f96c20e5b4d4bd8f444bfb3e29c8013c25c4e7c4
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-19 17:56:30 +0545

    learned about the extra statements

--- statement.js
+++ statement.js
@@ -1,56 +1,63 @@
-// // // // statements
-// // // // 1 .if else
-// // // // 2. switch case
+// // // // // statements
+// // // // // 1 .if else
+// // // // // 2. switch case
 
-// // // //if else syntax
-// // // if (condition){
-// // //     print result
+// // // // //if else syntax
+// // // // if (condition){
+// // // //     print result
+// // // // }
+// // // // else(condition)
+// // // // {
+// // // //     print result
+// // // // }
+
+// // // //switch case syntax
+// // // const gender = prompt("click 1 if you are male and 2 if you are female");
+// // // if (gender == 1
+
+// // // ) {
+// // //   alert("you are male");
+// // // } else {
+// // //   alert("you are female");
 // // // }
-// // // else(condition)
-// // // {
-// // //     print result
-// // // }
+// // // = variable assign
+// // // == data type and value assign
+// // // === data type and value assign + data conversion to bullen
 
-// // //switch case syntax
-// // const gender = prompt("click 1 if you are male and 2 if you are female");
-// // if (gender == 1
+// // //2 switch case..
 
-// // ) {
-// //   alert("you are male");
-// // } else {
-// //   alert("you are female");
+// // // syntax
+
+// // // switch(condition)
+// // {
+// //     case "1";
+// //     alert("sunday");
+// //     break;
+// //     case "2";
+// //     alert("monday");
+// //     break;
+// //     default:
+// //         alert("you must choose between 1 nad 7");
+
 // // }
-// // = variable assign
-// // == data type and value assign
-// // === data type and value assign + data conversion to bullen
 
-// //2 switch case..
+// //Wap to check grade of student  A grade
 
-// // syntax
-
-// // switch(condition)
-// {
-//     case "1";
-//     alert("sunday");
+// const per = Number(prompt("enter your percentage"));
+// switch (true) {
+//   case per > 80 && per <= 100:
+//     alert("your grade is A");
 //     break;
-//     case "2";
-//     alert("monday");
+//   case per >= 60 && per <= 80:
+//     alert("your grade is B");
 //     break;
-//     default:
-//         alert("you must choose between 1 nad 7");
-
+//   default:
+//     alert("better luck next time");
 // }
 
-//Wap to check grade of student  A grade
-
-const per = Number(prompt("enter your percentage"));
-switch (true) {
-  case per > 80 && per <= 100:
-    alert("your grade is A");
-    break;
-  case per >= 60 && per <= 80:
-    alert("your grade is B");
-    break;
-  default:
-    alert("better luck next time");
-}
+// conditional operator (ternary operator )
+// if()else es5
+// es6 ternary operator
+// eg
+const gender = "m";
+gender === "m" ? console.log("m") : console.log("f");
