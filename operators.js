387b9064b792ca8f1740e36896c51f9d562a5ed3
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-19 18:04:23 +0545

    learned about operators

---
+++ operators.js
@@ -0,0 +1,4 @@
+// there are three types of operators
+// 1 and (both conditions must be true) &&= symbol of and operator
+// 2 or (any one condition must satisfy) ||= symbol of or operator
+// 3 Not (makes value oppositive like if value is true it converts into false and reverse) != symbol of not operator
