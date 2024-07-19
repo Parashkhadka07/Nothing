8436e5a509788d84356654c6f7fd3646eaaa66f4
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-19 17:05:41 +0545

    learned about datatypes

---
+++ data-types.js
@@ -0,0 +1,18 @@
+// types of data types
+
+// 1. string
+
+// it is the text value in any Forrm
+//  eg:
+//  const p = "2";
+//  console.log("p");
+//  data types are 5
+//  1 primitive data types
+//  2 complex data ty7pes
+
+//  type conversion/ coerition
+//  eg const a="5";
+//      console.log(type of a);
+//      check type
+//      const p= Number(a); change type
+//      consloe.log(p);
