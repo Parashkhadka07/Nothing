553ac03283ded72444d714c18bea3e0e18d8b667
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-21 17:29:48 +0545

    learned about the loops

---
+++ loops.js
@@ -0,0 +1,35 @@
+// // // // // three kinds of loops
+// // // // // for
+// // // // // while loop
+// // // // // do while loop
+
+// // // // //syntax for for loop
+// // // // for(begain;  stepcondition;  increase begin)
+// // // //     {//code }
+
+// // // //wap a javascript program for table of any number
+
+// const t = Number(prompt("enter the number you want a table of"));
+
+// if(type of table != Number)
+// {
+//     for(let i=1; i<=10; i++)
+//     {
+//         const  (c=t*i);
+//         console.log(`${2}*${i}=${c}`);
+//     } }
+//     else{alert("please enter the valid nuber")}
+
+// // // // syntax for do while
+
+// // // //begin step
+// const t = Number(prompt("enter the number you want a table of"));
+
+// let i = 1;
+// do {
+//   let c = t * i;
+//   console.log(`${t}*${i}=${c}`);
+//   i++;
+// } while (i <= 10);
+
+// while
