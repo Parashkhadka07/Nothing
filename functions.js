9bbde309401f95b25e2f1e2e657b1f30e30ec222
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-22 17:56:14 +0545

    learned about functions

--- functions.js
+++ functions.js
@@ -22,3 +22,61 @@
 // //     return x*x ;}
 // const newanswer = square(4);
 // // console.log({ newanswer });
+
+/*types of function
+1. default function
+2.parametrized function
+3.arrow function(it is es6 function)
+4.closure
+5.IIFes
+6.recursive function
+7.inline function
+8.abonymous function
+9.callback functi0n
+10. pure funtion
+11.implicit funtion
+12.explicit function
+*/
+
+// default function is used for paganation
+// parametrized function
+// const sum({a,b,c})=>{
+//     console.log(a+b+c);
+// }
+// sum({a:2,b:3,c:5});
+
+// it helps to  fix the values of paramaters without caring about the SVGTextPositioningElement
+
+//arrow function is ES6 function
+
+//4.closure(most important)
+
+// have to call function from function
+// have to look at it properly
+
+//IIFes(immediately invoked function extression)
+//Devops for
+// ()=>{
+//     console
+// }
+
+//recursive function
+
+// const sum = (n) => {
+//   if (x <= 0) {
+//     alert("enter the valid data ");
+//   } else {
+//     let
+//     return sum();// have to be clear
+//   }
+// };
+// const x = Number(prompt("enter the number you want to count down of"));
+// console.log(`${sum(x)}`);
+
+
+// inline function
+//
+
+
+
+implicit and callback function use
