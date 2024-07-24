eefe085aec012e6e21214e626511dd08222efe86
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-24 11:00:21 +0545

    updated

--- string.js
+++ string.js
@@ -17,17 +17,40 @@
 
 // to check prime or not by using function
 
-const check = (num) => {
-  if (num <= 1) {
-    return `${num} is not a prime number`;
-  }
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-      return `${num} is not prime no`;
+// const check = (num) => {
+//   if (num <= 1) {
+//     return `${num} is not a prime number`;
+//   }
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return `${num} is not prime no`;
+//     }
+//   }
+//   return `${num} is  prime no`;
+// };
+
+// const number = Number(prompt("enter the number you wanna check"));
+// console.log(check(number));
+
+//to count certain charactr in string
+
+const countchar = (word, char) => {
+  let count = 0;
+  console.log(`${word.length}`);
+
+  const array = word.split("");
+  console.log(`${array}`);
+
+  for (let i = 0; i < word.length; i++) {
+    if (array[i] === char) {
+      count = count + 1;
     }
   }
-  return `${num} is  prime no`;
+  return count;
 };
 
-const number = Number(prompt("enter the number you wanna check"));
-console.log(check(number));
+const enteredstring = String(prompt("enter the word "));
+const chartocount = String(prompt("enter the character you wanna count "));
+console.log(
+  `${countchar(enteredstring.toLowerCase(), chartocount.toLowerCase())}`
+);
