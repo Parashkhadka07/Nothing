0314cd13ff12606382faba06ddaffe04da5b2786
Parshkhadka07 <parashkhadka020@gmail.com>
2024-07-24 17:48:19 +0545

    learned about the object

---
+++ object.js
@@ -0,0 +1,41 @@
+
+
+// objects (complex datatypes)
+//syntax
+cinst Object(anyname)={
+
+   
+   
+    (curly bracket to introduce object)
+name:"parash",//property(key value pair name is key parash is value)
+// and can use function as a prperty or value in any key
+//like 
+age: function age()P{
+    return 2024-Number(this.object name("-"[0]));
+in es5 this represents a whole object but  not in es6
+
+
+}
+}
+
+
+//object read or likr to print data
+
+console.log(objectname.any key you want to print or read);
+
+
+/// object update
+
+just have to write like reading data on left and new value to assign in right to update
+
+like
+objectname.keyofobject="new data like to replace name fro parash to ram have to write ram"
+
+//delete
+just have to write delete objectname.key(can be name age );
+
+//spread operator
+used to make duplicate object of original one
+
+syntax
+const{key,...anyname you want to give};
